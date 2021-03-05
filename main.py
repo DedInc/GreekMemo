676e6eb91448afd38aba84a8c039eaee826a0749
DedInc <41906303+DedInc@users.noreply.github.com>
2021-03-05 20:27:06 +0600

    Fixed unsopportable symbols & new console style

--- main.py
+++ main.py
@@ -3,7 +3,34 @@ from sys import exit
 from os import system, mkdir
 from codecs import open as openc
 from random import randint as rnt
-import psutil
+import psutil, ctypes
+
+def initConsole():
+	LF_FACESIZE = 32
+	STD_OUTPUT_HANDLE = -11
+
+	class COORD(ctypes.Structure):
+	    _fields_ = [("X", ctypes.c_short), ("Y", ctypes.c_short)]
+
+	class CONSOLE_FONT_INFOEX(ctypes.Structure):
+	    _fields_ = [("cbSize", ctypes.c_ulong),
+	                ("nFont", ctypes.c_ulong),
+	                ("dwFontSize", COORD),
+	                ("FontFamily", ctypes.c_uint),
+	                ("FontWeight", ctypes.c_uint),
+	                ("FaceName", ctypes.c_wchar * LF_FACESIZE)]
+
+	font = CONSOLE_FONT_INFOEX()
+	font.cbSize = ctypes.sizeof(CONSOLE_FONT_INFOEX)
+	font.nFont = 16
+	font.dwFontSize.X = 11
+	font.dwFontSize.Y = 18
+	font.FontFamily = 54
+	font.FontWeight = 400
+	font.FaceName = "Consolas"
+
+	handle = ctypes.windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+	ctypes.windll.kernel32.SetCurrentConsoleFontEx(handle, ctypes.c_long(False), ctypes.pointer(font))
 
 def getPython():
 	procs = psutil.pids()
@@ -31,6 +58,7 @@ except:
 		print('Failed :(')
 		system('pause')
 		exit()
+initConsole()
 init()
 
 def importCards(file):
